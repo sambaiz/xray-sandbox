608caf87468627f6cb58581f8e3339c088f2f9be
sambaiz <godgourd@gmail.com>
2021-08-06 00:36:16 +0900

    use middleware

--- main.go
+++ main.go
@@ -20,43 +20,43 @@ func main() {
 	}
 
 	e := echo.New()
-	hello := echo.WrapHandler(xray.Handler(xray.NewFixedSegmentNamer("test-app"), http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-
-		// http request
-		req, err := http.NewRequest(http.MethodGet, "https://aws.amazon.com/", nil)
-		if err != nil {
-			w.Write([]byte(err.Error()))
-			return
-		}
-		resp, err := xray.Client(http.DefaultClient).Do(req.WithContext(ctx))
-		if err != nil {
-			w.Write([]byte(err.Error()))
-			return
-		}
-		io.Copy(io.Discard, resp.Body)
-		resp.Body.Close()
-
-		// sub segment
-		subCtx, subSeg := xray.BeginSubsegment(ctx, "waiting-something")
-
-		// aws sdk
-		cfg, err := config.LoadDefaultConfig(subCtx)
-		if err != nil {
-			w.Write([]byte(err.Error()))
-			return
-		}
-		awsv2.AWSV2Instrumentor(&cfg.APIOptions)
-		svc := s3.NewFromConfig(cfg)
-		if _, err := svc.ListBuckets(subCtx, &s3.ListBucketsInput{}); err != nil {
-			w.Write([]byte(err.Error()))
-			return
-		}
-
-		subSeg.Close(nil)
-
-		w.Write([]byte("Hello!"))
-	})))
+	e.Use(echo.WrapMiddleware(func(h http.Handler) http.Handler {
+		return xray.Handler(xray.NewFixedSegmentNamer("test-app"), h)
+	}))
 	e.GET("/", hello)
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func hello(c echo.Context) error {
+	ctx := c.Request().Context()
+	// http request
+	req, err := http.NewRequest(http.MethodGet, "https://aws.amazon.com/", nil)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	resp, err := xray.Client(http.DefaultClient).Do(req.WithContext(ctx))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	io.Copy(io.Discard, resp.Body)
+	resp.Body.Close()
+
+	// sub segment
+	subCtx, subSeg := xray.BeginSubsegment(ctx, "waiting-something")
+
+	// aws sdk
+	cfg, err := config.LoadDefaultConfig(subCtx)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	awsv2.AWSV2Instrumentor(&cfg.APIOptions)
+	svc := s3.NewFromConfig(cfg)
+	if _, err := svc.ListBuckets(subCtx, &s3.ListBucketsInput{}); err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return nil
+	}
+
+	subSeg.Close(nil)
+
+	return c.JSON(http.StatusOK, "hello")
 }
